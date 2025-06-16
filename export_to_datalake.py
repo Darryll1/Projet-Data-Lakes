8dc6c8e369375afe3ae2c2936d09e675d67323f6
Darryll <122524196+Darryll1@users.noreply.github.com>
2025-06-16 16:51:33 +0200

    export_to_datalake.py

--- export_to_datalake.py
+++ export_to_datalake.py
@@ -14,7 +14,7 @@ from datetime import datetime
 import json
 import uuid
 
-print("📤 Export vers Data Lake (MinIO)...")
+print("Export vers Data Lake (MinIO)...")
 
 # Connexion MinIO
 s3 = boto3.client(
@@ -36,7 +36,7 @@ df_pg = pd.read_sql("SELECT * FROM mesures", conn)
 csv_pg = "/tmp/postgres_mesures.csv"
 df_pg.to_csv(csv_pg, index=False)
 s3.upload_file(csv_pg, bucket_name, f"structured/postgres_mesures_{datetime.now().isoformat()}.csv")
-print("✅ PostgreSQL exporté")
+print("PostgreSQL exporté")
 
 # === MongoDB (semi-structured) ===
 mongo = MongoClient("mongodb://localhost:27017/")
@@ -44,7 +44,7 @@ docs = list(mongo.smartcity_documents.mesures.find({}, {"_id": 0}))
 with open("/tmp/mongo_mesures.json", "w") as f:
     json.dump(docs, f)
 s3.upload_file("/tmp/mongo_mesures.json", bucket_name, f"semi_structured/mongo_mesures_{datetime.now().isoformat()}.json")
-print("✅ MongoDB exporté")
+print("MongoDB exporté")
 
 # === Cassandra (semi-structured) ===
 cluster = Cluster(["127.0.0.1"], port=9042)
@@ -64,7 +64,7 @@ with open("/tmp/cassandra_mesures.json", "w") as f:
     json.dump(cass_data, f, indent=2)
 
 s3.upload_file("/tmp/cassandra_mesures.json", bucket_name, f"semi_structured/cassandra_mesures_{datetime.now().isoformat()}.json")
-print("✅ Cassandra exporté")
+print("Cassandra exporté")
 
 
 # === Neo4j (semi-structured) ===
@@ -76,4 +76,4 @@ with driver.session() as session:
 with open("/tmp/neo4j_mesures.json", "w") as f:
     json.dump(neo_data, f)
 s3.upload_file("/tmp/neo4j_mesures.json", bucket_name, f"semi_structured/neo4j_mesures_{datetime.now().isoformat()}.json")
-print("✅ Neo4j exporté")
+print("Neo4j exporté")
